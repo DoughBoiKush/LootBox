6637704a3e517a42a4cada82a1627de49b80f1af
SanityCentral <38260799+SanityCentral@users.noreply.github.com>
2018-04-26 13:19:03 -0500

    Update LootBox.java

--- lootbox/LootBox.java
+++ lootbox/LootBox.java
@@ -1,24 +1,30 @@
+
 package com.mcs270.lootbox;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class LootBox extends AppCompatActivity {
 
-    private Button mNewGameButton;
+    //private Button mNewGameButton;
     private Button mContinueGameButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loot_box);
-    }
 
-    @Override
-    public boolean
-    Intent intent = CrimePagerActivity.newIntent(getActivity(),crime.getId());
-    startActivity(intent);
+        mContinueGameButton = (Button) findViewById(R.id.continue_game_button);
+        mContinueGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LootBoxActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 
 }
