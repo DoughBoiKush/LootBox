5c55b79912cc5a4980d800a6740ec32eaaae2028
SanityCentral <38260799+SanityCentral@users.noreply.github.com>
2018-04-26 13:19:52 -0500

    Update LootBoxActivity.java

--- lootbox/LootBoxActivity.java
+++ lootbox/LootBoxActivity.java
@@ -1,21 +1,14 @@
 package com.mcs270.lootbox;
 
-import android.support.v4.view.ViewPager;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
 
-/**
- * Created by Kelsan on 4/17/18.
- */
 
-public class LootBoxActivity extends AppCompatActivity {
+public class LootBoxActivity extends AppCompatActivity{
 
-    private Button mButton1;
-    private Button mButton2;
-    private Button mButton3;
-    private Button mButton4;
-    private Button mBox;
-
-    private ViewPager mViewPager;
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_layout);
+    }
 }
