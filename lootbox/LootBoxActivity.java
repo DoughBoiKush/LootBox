7cf31372e18e6463030125cc723c025f534b2dd9
SanityCentral <38260799+SanityCentral@users.noreply.github.com>
2018-04-26 19:24:17 -0500

    Update LootBoxActivity.java

--- lootbox/LootBoxActivity.java
+++ lootbox/LootBoxActivity.java
@@ -1,14 +1,29 @@
 package com.mcs270.lootbox;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 
 public class LootBoxActivity extends AppCompatActivity{
+
+    private Button mInventoryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_layout);
+
+        mInventoryButton = (Button) findViewById(R.id.inventory_button);
+        mInventoryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LootBoxInventory.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
