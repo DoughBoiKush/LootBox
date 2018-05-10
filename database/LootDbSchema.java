f3622a821fe1e1442560b47020f0363f7956a9fc
SanityCentral <38260799+SanityCentral@users.noreply.github.com>
2018-05-10 12:51:02 -0500

    Update LootDbSchema.java

--- database/LootDbSchema.java
+++ database/LootDbSchema.java
@@ -1,15 +1,12 @@
 package com.mcs270.lootbox.database;
 
-/**
- * Created by Kelsan on 4/12/18.
- */
-
 public class LootDbSchema {
     public static final class LootTable {
         public static final String NAME = "loot";
 
         public static final class Cols {
             public static final String UUID = "uuid";
+            public static final String NUM = "num";
             public static final String TIER = "tier";
             public static final String RARITY = "rarity";
             public static final String PRICE = "price";
