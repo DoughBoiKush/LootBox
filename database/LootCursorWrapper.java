0ff96239dc7938937f3c3fee9c59378af0a4a835
SanityCentral <38260799+SanityCentral@users.noreply.github.com>
2018-05-10 12:50:26 -0500

    Update LootCursorWrapper.java

--- database/LootCursorWrapper.java
+++ database/LootCursorWrapper.java
@@ -7,10 +7,6 @@ import com.mcs270.lootbox.Box;
 
 import java.util.UUID;
 
-/**
- * Created by Kelsan on 5/1/18.
- */
-
 public class LootCursorWrapper extends CursorWrapper{
 
         public LootCursorWrapper(Cursor cursor) {
@@ -19,12 +15,14 @@ public class LootCursorWrapper extends CursorWrapper{
 
         public Box getBox() {
             String uuidString = getString(getColumnIndex(LootDbSchema.LootTable.Cols.UUID));
+            int num = getInt(getColumnIndex(LootDbSchema.LootTable.Cols.NUM));
             String tier = getString(getColumnIndex(LootDbSchema.LootTable.Cols.TIER));
             String rarity = getString(getColumnIndex(LootDbSchema.LootTable.Cols.RARITY));
             int isKept = getInt(getColumnIndex(LootDbSchema.LootTable.Cols.KEPT));
             String price = getString(getColumnIndex(LootDbSchema.LootTable.Cols.PRICE));
 
             Box box = new Box(UUID.fromString(uuidString));
+            box.setNum(num);
             box.setTier(tier);
             box.setRarity(rarity);
             box.setKept(isKept != 0);
@@ -34,3 +32,4 @@ public class LootCursorWrapper extends CursorWrapper{
         }
 
 }
+
