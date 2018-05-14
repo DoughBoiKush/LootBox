7c278be7f1fa475924720596ba6b2cf96ea7f916
Kelsan1000 <38260819+Kelsan1000@users.noreply.github.com>
2018-05-14 17:00:45 -0500

    Update LootBaseHelper.java

--- database/LootBaseHelper.java
+++ database/LootBaseHelper.java
@@ -5,30 +5,31 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import com.mcs270.lootbox.database.LootDbSchema.LootTable;
 
-    public class LootBaseHelper extends SQLiteOpenHelper {
-        private static final int VERSION = 1;
-        private static final String DATABASE_NAME = "crime.db";
+public class LootBaseHelper extends SQLiteOpenHelper {
+    private static final int VERSION = 1;
+    private static final String DATABASE_NAME = "crime.db";
 
-        public LootBaseHelper(Context context) {
-            super(context, DATABASE_NAME, null, VERSION);
-        }
-
-        @Override
-        public void onCreate(SQLiteDatabase db) {
-            db.execSQL("create table " + LootTable.NAME + "(" +
-                    " _id integer primary key autoincrement, " +
-                    LootTable.Cols.UUID + ", " +
-                    LootTable.Cols.NUM + "," +
-                    LootTable.Cols.TIER + ", " +
-                    LootTable.Cols.RARITY + ", " +
-                    LootTable.Cols.PRICE + ", " +
-                    LootTable.Cols.KEPT +
-                    ")"
-            );
-        }
-
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-
-        }
+    public LootBaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
     }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + LootTable.NAME + "(" +
+                " _id integer primary key autoincrement, " +
+                LootTable.Cols.UUID + ", " +
+                LootTable.Cols.NUM + "," +
+                LootTable.Cols.TIER + ", " +
+                LootTable.Cols.RARITY + ", " +
+                LootTable.Cols.PRICE + ", " +
+                LootTable.Cols.KEPT +
+                ")"
+        );
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+
+    }
+}
